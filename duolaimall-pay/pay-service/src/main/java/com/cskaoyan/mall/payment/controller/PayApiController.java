a93857e8334b5526e4b79eb0a88d1552bf76b5db
zyx <zyx3315885375@gmail.com>
2024-02-02 20:28:02 +0800

    feat: Add payIndex and submitOrder methods

--- duolaimall-pay/pay-service/src/main/java/com/cskaoyan/mall/payment/controller/PayApiController.java
+++ duolaimall-pay/pay-service/src/main/java/com/cskaoyan/mall/payment/controller/PayApiController.java
@@ -1,2 +1,22 @@
-package com.cskaoyan.mall.payment.controller;public class PayApiController {
+package com.cskaoyan.mall.payment.controller;
+
+import com.cskaoyan.mall.common.result.Result;
+import com.cskaoyan.mall.order.dto.OrderInfoDTO;
+import com.cskaoyan.mall.payment.client.OrderApiClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PayApiController {
+    @Autowired
+    OrderApiClient orderApiClient;
+    @GetMapping("pay/auth")
+    public Result<OrderInfoDTO> payIndex(Long orderId){
+        // 调用订单服务获取订单信息
+        OrderInfoDTO orderInfoDTO = orderApiClient.getOrderInfoDTO(orderId);
+        return Result.ok(orderInfoDTO);
+    }
+
 }
--- duolaimall-pay/pay-service/src/main/java/com/cskaoyan/mall/payment/controller/PayController.java
+++ duolaimall-pay/pay-service/src/main/java/com/cskaoyan/mall/payment/controller/PayController.java
@@ -1,2 +1,27 @@
-package com.cskaoyan.mall.payment.controller;public class PayController {
+package com.cskaoyan.mall.payment.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/pay")
+public class PayController {
+    @RequestMapping("alipay/submit/{orderId}")
+    @ResponseBody
+    public String submitOrder(@PathVariable Long orderId){
+
+        //String form = alipayService.createAliPay(orderId);
+        // 在上面这个支付的方法中，需要做的事情有如下几个
+
+        // 1. 校验支付对应的订单状态是否为未支付，如果是已支付或已关闭，则直接返回
+
+        // 2. 保存支付记录到支付表
+
+        // 3. 调用支付宝SDK，生成支付表单（支付表单实际上就是一个支付页面，是一个html的字符串）
+
+        // 4. 返回支付表单
+        return null;
+    }
 }
